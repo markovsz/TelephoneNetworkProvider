65cae8cef4969967c895bb1c5ec19d600bdec059
Stas <markovszzzz@gmail.com>
2022-04-05 20:35:31 +0300

    Operator repository completed

---
+++ Repository/OperatorRepository/ICallRepositoryForOperator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Entities.RequestFeatures;
+
+namespace Repository
+{
+    public interface ICallRepositoryForOperator
+    {
+        IEnumerable<Call> GetCalls(CallParameters parameters, bool trackChanges);
+        void CreateCall(Call call); 
+        void DeleteCallById(uint id); 
+    }
+}
