6ee2ca679aa74a61fb24392dbe7d621c244efa59
Stas <markovszzzz@gmail.com>
2022-04-05 20:41:18 +0300

    Authentication manager added

---
+++ Repository/IAuthenticationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+namespace Repository
+{
+    public interface IAuthenticationManager
+    {
+        Task<bool> ValidateUser(UserForAuthenticationDto user);
+        Task<string> CreateToken();
+    }
+}
