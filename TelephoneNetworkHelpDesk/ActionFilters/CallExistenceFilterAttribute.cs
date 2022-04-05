f58004f1f655e9984aacb0bf1129b7971a34481a
Stas <markovszzzz@gmail.com>
2022-04-05 20:48:15 +0300

    action filters added

---
+++ TelephoneNetworkHelpDesk/ActionFilters/CallExistenceFilterAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BussinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelephoneNetworkProvider.ActionFilters
+{
+    public class CallExistenceFilterAttribute : IActionFilter
+    {
+        private AdministratorLogic _administratorLogic;
+
+        public CallExistenceFilterAttribute(AdministratorLogic administratorLogic)
+        {
+            _administratorLogic = administratorLogic;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var action = context.RouteData.Values["action"];
+            var controller = context.RouteData.Values["controller"];
+            var customerId = (uint)context.ActionArguments["id"];
+            bool isExist = _administratorLogic.CheckCall(customerId);
+            if (isExist)
+                context.Result = new BadRequestObjectResult($"In {controller}.{action}, call with id = {customerId} not found");
+        }
+    }
+}
---
+++ TelephoneNetworkHelpDesk/ActionFilters/CustomerExistenceFilterAttribute.cs
@@ -0,0 +1,35 @@
+﻿using BussinessLogic;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelephoneNetworkProvider.ActionFilters
+{
+    public class CustomerExistenceFilterAttribute : IActionFilter
+    {
+        private IAdministratorLogic _administratorLogic;
+
+        public CustomerExistenceFilterAttribute(IAdministratorLogic administratorLogic)
+        {
+            _administratorLogic = administratorLogic;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var action = context.RouteData.Values["action"];
+            var controller = context.RouteData.Values["controller"];
+            var customerId = (uint)context.ActionArguments["id"];
+            bool isExist = _administratorLogic.CheckCustomer(customerId);
+            if (isExist)
+                context.Result = new BadRequestObjectResult($"In {controller}.{action}, customer with id = {customerId} not found");
+        }
+    }
+}
