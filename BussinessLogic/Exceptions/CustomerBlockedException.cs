518cea161dcf844aec321096a4f433d6ba434f16
Stas <markovszzzz@gmail.com>
2022-05-12 18:32:12 +0300

    bussiness logic exceptions added

---
+++ BussinessLogic/Exceptions/CustomerBlockedException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.Exceptions
+{
+    public class CustomerBlockedException : Exception
+    {
+        public CustomerBlockedException()
+        {
+        }
+
+        public CustomerBlockedException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ BussinessLogic/Exceptions/InvalidUserDataException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.Exceptions
+{
+    public class InvalidUserDataException : Exception
+    {
+        public InvalidUserDataException(string message)
+            : base(message)
+        {
+        }
+    }
+}
